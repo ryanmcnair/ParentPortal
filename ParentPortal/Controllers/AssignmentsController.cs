ec256fc9a45473841a7d53ea8ddb17a35689d9ec
Ryan McNair <ryan.mcnair@gmail.com>
2021-05-28 16:34:33 -0500

    assignments api is ready to rock n roll

--- ParentPortal/Controllers/AssignmentsController.cs
+++ ParentPortal/Controllers/AssignmentsController.cs
@@ -25,5 +25,54 @@ namespace ParentPortal.Controllers
         {
             return Ok(_repo.GetAll());
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var assignment = _repo.Get(id);
+            if (assignment == null)
+            {
+                return NotFound("This user id does not exist");
+            }
+            return Ok(assignment);
+        }
+
+        [HttpGet("classroom/{id}")]
+        public IActionResult GetAssignmentByClassroom(int id)
+        {
+            var assignment = _repo.GetAssignmentByClassroom(id);
+            if (assignment == null)
+            {
+                return NotFound("This user id does not exist");
+            }
+            return Ok(assignment);
+        }
+
+        [HttpPost]
+        public IActionResult AddAssignment(Assignment assignment)
+        {
+            _repo.Add(assignment);
+            return Created($"api/assignments/{assignment.id}", assignment);
+        }
+
+        [HttpDelete("{assignmentid}")]
+        public IActionResult DeleteMessage(int assignmentid)
+        {
+            _repo.Remove(assignmentid);
+            return Ok();
+        }
+
+        [HttpPatch("{id}/update")]
+        public IActionResult UpdateUser(int id, Assignment assignmentObj)
+        {
+            var assignment = _repo.Get(id);
+
+            assignment.pdf_url = assignmentObj.pdf_url;
+            assignment.date_due = assignmentObj.date_due;
+            assignment.text = assignmentObj.text;
+
+            _repo.Update(assignment);
+            return NoContent();
+        }
     }
 }
--- ParentPortal/DataAccess/AssignmentRepository.cs
+++ ParentPortal/DataAccess/AssignmentRepository.cs
@@ -22,5 +22,72 @@ namespace ParentPortal.DataAccess
 
             return db.Query<Assignment>(sql).ToList();
         }
+
+        public Assignment Get(int id)
+        {
+            var sql = @"SELECT *
+                        FROM assignment
+                        WHERE id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            return db.QueryFirstOrDefault<Assignment>(sql, new { id = id });
+
+        }
+
+        public List<Assignment> GetAssignmentByClassroom(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"SELECT *
+                        FROM assignment
+                            WHERE classroom_id = @id";
+
+            return db.Query<Assignment>(sql, new { id = id }).ToList();
+        }
+
+        public void Add(Assignment assignment)
+        {
+            var sql = @"INSERT INTO [dbo].[assignment]
+                        ([classroom_id]
+                        ,[teacher_id]
+                        ,[pdf_url]
+                        ,[date_added]
+                        ,[date_due]
+                        ,[text])
+                        OUTPUT INSERTED.id 
+                        VALUES
+                        (@classroom_id, @teacher_id, @pdf_url, CURRENT_TIMESTAMP, @date_due, @text)";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var id = db.ExecuteScalar<int>(sql, assignment);
+
+            assignment.id = id;
+        }
+
+        public void Remove(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"DELETE
+                        FROM assignment
+                        WHERE id = @id";
+
+            db.Execute(sql, new { id });
+        }
+
+        public void Update(Assignment assignment)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"UPDATE assignment
+                        SET pdf_url = @pdf_url,
+                        date_due = @date_due,
+                        text = @text
+                        WHERE id = @id";
+
+            db.Execute(sql, assignment);
+        }
     }
 }
