848ba3f249742b75160d3adb0c93636fe8f97052
Ryan McNair <ryan.mcnair@gmail.com>
2021-05-28 13:28:48 -0500

    announcements api is ready to go

--- ParentPortal/DataAccess/AnnouncementRepository.cs
+++ ParentPortal/DataAccess/AnnouncementRepository.cs
@@ -22,5 +22,62 @@ namespace ParentPortal.DataAccess
 
             return db.Query<Announcement>(sql).ToList();
         }
+
+        public Announcement Get(int id)
+        {
+            var sql = @"SELECT *
+                        FROM announcement
+                        WHERE id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            return db.QueryFirstOrDefault<Announcement>(sql, new { id = id });
+
+        }
+
+        public void Add(Announcement announcement)
+        {
+            var sql = @"INSERT INTO [dbo].[announcement]
+                        ([school_id]
+                        ,[publisher_id]
+                        ,[pdf_url]
+                        ,[date_added]
+                        ,[text]
+                        ,[staff_only])
+                        OUTPUT INSERTED.id 
+                        VALUES
+                        (@school_id, @publisher_id, @pdf_url, CURRENT_TIMESTAMP, @text, @staff_only)";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var id = db.ExecuteScalar<int>(sql, announcement);
+
+            announcement.id = id;
+        }
+
+        public void Remove(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"DELETE
+                        FROM announcement
+                        WHERE id = @id";
+
+            db.Execute(sql, new { id });
+        }
+
+        public void Update(Announcement announcement)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"UPDATE announcement
+                        SET publisher_id = @publisher_id,
+                            pdf_url = @pdf_url,
+                            text = @text,
+                            staff_only = @staff_only
+                        WHERE id = @id";
+
+            db.Execute(sql, announcement);
+        }
     }
 }
