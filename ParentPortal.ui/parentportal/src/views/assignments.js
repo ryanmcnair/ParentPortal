bb219894792f775a200e58370c71e2965a793273
Ryan McNair <ryan.mcnair@gmail.com>
2021-06-04 16:10:10 -0500

    a few small updates to home and got the assignments posting in state by the users classroom

--- ParentPortal.ui/parentportal/src/views/assignments.js
+++ ParentPortal.ui/parentportal/src/views/assignments.js
@@ -1,7 +1,24 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import assignmentData from '../helpers/data/assignmentData';
 
 export default class Assignments extends React.Component {
-    state = {};
+    state = {
+      dbUser: this.props.dbUser,
+      assments: []
+    };
+
+    componentDidMount() {
+      this.getClassroomAssignments();
+    }
+
+    getClassroomAssignments = () => {
+      assignmentData.getAllAssignmentsByClassroom(this.state.dbUser.classroom_id).then((response) => {
+        this.setState({
+          assments: response
+        });
+      });
+    }
 
     render() {
       return (
---
+++ ParentPortal.ui/parentportal/src/views/home.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+export default class Home extends React.Component {
+    state = { };
+
+    render() {
+      return (
+            <>
+            <h1>Home View</h1>
+            {/* <h2>Welcome, {this.props.dbUser.first_name}!</h2> */}
+            </>
+      );
+    }
+}
